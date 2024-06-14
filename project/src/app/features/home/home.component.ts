d6a7f9634c5c3c963c9ade2fe4842ed55032ed87
Carlos N <machinery.e@gmail.com>
2024-06-14 12:29:56 +0000

    Crear y configurar servicio para agregar script

--- project/src/app/features/home/home.component.ts
+++ project/src/app/features/home/home.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, Renderer2 } from '@angular/core';
 import { Subject } from 'rxjs';
+import { pixelScript } from '../../../assets';
+import { HomeSandboxService } from './home-sandbox.service';
+
+const SCRIPT_PATH = "/assets/scripts/pixel.script.ts";
 
 @Component({
   selector: 'app-home',
@@ -10,6 +14,32 @@ export class HomeComponent implements OnDestroy {
    * Component destroyed stream
    */
   componentDestroyed$: Subject<void> = new Subject<void>();
+  /**
+   * Constructor del componente
+   * @param renderer 
+   * @param sandbox 
+   */
+  constructor(
+    private renderer: Renderer2,
+    private sandbox: HomeSandboxService
+  ) {}
+  /**
+   * Despues de iniciar la vista
+   */
+  ngAfterViewInit() {
+    const text = pixelScript('12312312312321');
+    const scriptElement = this.sandbox.loadScript(
+      this.renderer,
+      SCRIPT_PATH,
+      text
+    );
+    scriptElement.onload = () => {
+      console.log("Google API Script loaded");
+    };
+    scriptElement.onerror = () => {
+      console.log("Could not load the Google API Script!");
+    };
+  }
   /**
    * Destructor del componente
    */
--- project/src/app/features/home/home.module.ts
+++ project/src/app/features/home/home.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home.routes';
+import { HomeSandboxService } from './home-sandbox.service';
 
 @NgModule({
   imports: [CommonModule, HomeRoutingModule],
   declarations: [HomeComponent],
   exports: [],
+  providers: [HomeSandboxService],
 })
 export class HomeModule {}
